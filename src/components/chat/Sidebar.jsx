d7effe56c943641aaeffd1fc2016ca6d6a6b10dd
Javi <javiermespino@gmail.com>
2023-04-09 20:23:45 +0000

    Commented line with errors

--- src/components/chat/Sidebar.jsx
+++ src/components/chat/Sidebar.jsx
@@ -74,9 +74,9 @@ function Sidebar() {
             value={searchTerm}
             onChange={handleSearchInputChange}
           />
-          <button className="m-1 btn btn-circle">
+          {/* <button className="m-1 btn btn-circle">
             <svg class="h-6 w-6 text-black-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="10" cy="10" r="7" />  <line x1="21" y1="21" x2="15" y2="15" /></svg>
-          </button>
+          </button> */}
         </div>
       </div>
       <div className="flex-1">
