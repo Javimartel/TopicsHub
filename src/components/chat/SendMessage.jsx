63bbd923734305721a497d2a674023486cdc5800
Javi <javiermespino@gmail.com>
2023-04-12 12:12:30 +0000

    Fixed onChange with useRef hook on input

--- src/components/chat/SendMessage.jsx
+++ src/components/chat/SendMessage.jsx
@@ -1,34 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {auth, db} from "../../firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const SendMessage = ({theme}) => {
-    const [input, setInput] = useState('');
+    const inputRef = useRef('');
+  
     const sendMessage = async (e) => {
-        e.preventDefault()
-        if (input === '') {
-            alert('Please enter a message')
-            return;
-        };
-        const {uid, displayName} = auth.currentUser;
-        const textToAdd = input.trim();
-        setInput('');
-        await addDoc(collection(db, theme), {
-            text: textToAdd,
-            name: displayName,
-            uid,
-            timestamp: serverTimestamp(),
-        })
-    }
-
+      e.preventDefault();
+      const textToAdd = inputRef.current.value.trim();
+      console.log(textToAdd);
+      if (textToAdd === '') {
+        alert('Por favor añade un mensaje');
+        return;
+      }
+  
+      const {uid, displayName} = auth.currentUser;
+      await addDoc(collection(db, theme), {
+        text: textToAdd,
+        name: displayName,
+        uid,
+        timestamp: serverTimestamp(),
+      });
+  
+      // Limpiar el campo de entrada y restablecer el estado de error
+      inputRef.current.value = '';
+    };
+  
     return (
-        <div className="w-[50%] m-4">
-            <form onSubmit={sendMessage} className="flex justify-evenly">
-                <input  type="text" value={input} onChange={(e) => setInput(e.target.value)} className="input input-bordered input-info w-full max-w-xs" />
-                <button type="submit" className="btn btn-primary">Enviar</button>
-            </form>
-        </div>
+      <div className="w-[50%] m-4">
+        <form onSubmit={sendMessage} className="flex justify-evenly">
+            <input type="text" ref={inputRef} className="input input-bordered input-info w-full max-w-xs" />
+            <button type="submit" className="btn btn-primary">Enviar</button>
+        </form>
+      </div>
     );
-}
+  };
 
 export default SendMessage;
