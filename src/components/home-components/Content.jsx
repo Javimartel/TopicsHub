5878f6de8fa82ffc625c6292e6f51d6cac1e84ad
PhPloveerPhP <marioperezmarrero@gmail.com>
2023-04-20 09:28:08 +0200

    Retouches

--- src/components/home-components/Content.jsx
+++ src/components/home-components/Content.jsx
@@ -99,31 +99,31 @@ export default function Content() {
                             </div>
                         </div>
                     </div>
-
+                    
                     <div className="hidden select-none 2xl:block 2xl:w-1/3"> {/* Chat */}
                         <div className="chat chat-start">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                Hey, have you heard about TopicsHub? &#128064;
+                                <p>Hey, have you heard about TopicsHub? &#128064;</p>
                             </div>
                         </div>
                         <div className="chat chat-end">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                No &#128517;, I haven't heard of it.
+                                <p>No &#128517;, I haven't heard of it.</p>
                             </div>
                         </div>
                         <div className="chat chat-start">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                Oh, really? &#128533;
+                                <p>Oh, really? &#128533;</p>
                             </div>
                         </div>
                         <div className="chat chat-start">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                There's a wide range of topics, from politics and current <br />events to sports and entertainment.
+                                <p>There's a wide range of topics, from politics and current <br />events to sports and entertainment.</p>
                             </div>
                         </div>
                         <div className="chat chat-end">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                That sounds interesting. &#128527;
+                                <p>That sounds interesting. &#128527;</p>
                             </div>
                         </div>
                     </div>
@@ -131,21 +131,18 @@ export default function Content() {
             </section>
 
             <section className="flex flex-col mt-12"> {/* Features */}
-
                 <div className="flex flex-col items-center"> {/* Features Cont */}
-
                     <div className="w-full p-5 bg-[#fbf9fa] flex flex-col items-center"> {/* Features Title */}
-                        <h1 className="font-mono text-3xl font-bold text-center">
+                        <h2 className="font-mono text-3xl font-bold text-center">
                             ¿Por qué Topcis<span className="text-blue-500">Hub</span>?
-                        </h1>
-
+                        </h2>
                         <p className="mt-2 font-mono text-center text-md">
                             Un lugar donde puedes debatir y discutir temas de actualidad.
                             <br />
                             Siente la libertad de expresarte y compartir tus opiniones.
                         </p>
                     </div>
-
+                    
                     <div className="flex flex-wrap items-center w-2/3 mt-12 text-center md:justify-evenly lg:hidden md:w-full md:flex-nowrap md:pl-5 md:pr-5">{features}</div>
                     <div className="items-center hidden w-full mt-12 justify-evenly lg:flex ">{lg_features}</div>
                 </div>
@@ -154,9 +151,9 @@ export default function Content() {
             <section className="flex flex-col mt-12"> {/* What is TopicsHub */}
 
                 <div className="flex flex-col items-center p-5 bg-[#fbf9fa] w-full"> {/* What is TopicsHub Tittle */}
-                    <h1 className="font-mono text-3xl font-bold">
+                    <h2 className="font-mono text-3xl font-bold">
                         ¿Qué es  Topics<span className="text-blue-500">Hub</span>?
-                    </h1>
+                    </h2>
                     <p className="mt-2 font-mono text-center text-md">
                         Topics<span className="text-blue-500">Hub</span> es una plataforma de chat en tiempo real.
                         <br />
@@ -166,9 +163,9 @@ export default function Content() {
 
                 <div className="flex flex-col items-center mt-12 lg:justify-evenly lg:flex-row">
                     <div className="flex flex-col w-2/3 lg:w-1/3">
-                        <h2 className="font-mono text-2xl font-bold text-center">
+                        <h3 className="font-mono text-2xl font-bold text-center">
                             ¿Qué es  Topics<span className="text-blue-500">Hub</span>?
-                        </h2>
+                        </h3>
                         <p className="mt-2 font-mono text-justify text-black">
                             Topics<span className="text-blue-500">Hub</span> fue creado por una pareja de estudiantes de el IES Ana Luisa Benitez.
                             Esta idea surgió de la necesidad de poder chatear con personas de todo el mundo, con el fin de poder socializar y conocer gente nueva.
@@ -177,9 +174,9 @@ export default function Content() {
                     </div>
 
                     <div className="flex flex-col w-2/3 mt-8 lg:w-1/3 lg:mt-0">
-                        <h2 className="font-mono text-2xl font-bold text-center">
+                        <h3 className="font-mono text-2xl font-bold text-center">
                             ¿Cómo lo <span className="text-blue-500">creamos</span>?
-                        </h2>
+                        </h3>
                         <p className="mt-2 font-mono text-justify text-black">
                             Topics<span className="text-blue-500">Hub</span> fue creado por una pareja de estudiantes de el IES Ana Luisa Benitez.
                             Esta idea surgió de la necesidad de poder chatear con personas de todo el mundo, con el fin de poder socializar y conocer gente nueva.
@@ -192,9 +189,9 @@ export default function Content() {
             <section className="flex flex-col mt-12"> {/* Where to Chat */}
 
                 <div className="flex flex-col items-center p-5 bg-[#fbf9fa] w-full"> {/* Where to Chat Tittle */}
-                    <h1 className="font-mono text-3xl font-bold">
+                    <h2 className="font-mono text-3xl font-bold">
                         ¿Where to <span className="text-blue-500">Chat</span>?
-                    </h1>
+                    </h2>
                     <p className="font-mono text-center text-md">
                         Topics<span className="text-blue-500">Hub</span> es una plataforma de chat en tiempo real.
                         <br />
@@ -207,31 +204,31 @@ export default function Content() {
                     <div className="hidden select-none 2xl:block 2xl:w-1/3">
                         <div className="chat chat-start">
                             <div className="text-white bg-[#2196f3] chat-bubble">
-                                Hi, I'm TopicsHub! &#128075;
+                                <p>Hi, I'm TopicsHub! &#128075;</p>
                             </div>
                         </div>
                         <div className="chat chat-end">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                Hi, what is TopicsHub? &#128528;
+                                <p>Hi, what is TopicsHub? &#128528;</p>
                             </div>
                         </div>
                         <div className="chat chat-start">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                TopicsHub is a chat platform where you can talk about any topic you want.
+                                <p>TopicsHub is a chat platform where you can talk about any topic you want.</p>
                             </div>
                         </div>
                         <div className="chat chat-end">
                             <div className="text-white chat-bubble bg-[#2196f3]">
-                                Oh, that's cool! &#128513;
+                                <p>Oh, that's cool! &#128513;</p>
                             </div>
                         </div>
                     </div>
                     <div className="flex flex-col items-center justify-center w-4/5 h-full lg:justify-start lg:w-3/5 xl:w-3/5 xl:justify-center 2xl:w-1/3"> {/* Chart */}
-                        <Typography color="black" className="p-4 mt-2 font-mono text-center text-md lg:text-lg lg:w-2/3">
+                        <p className="p-4 mt-2 font-mono text-justify text-black text-md lg:text-lg lg:w-2/3">
                             Their are a lot of <span className="text-blue-500">Topics</span> to choose from, and you can create your own topic.
                             Just click on the button below and start chatting.
-                        </Typography>
-                        <Button color="light-blue" size="lg" className="w-1/3 mt-4">
+                        </p>
+                        <Button color="light-blue" size="lg" className="w-1/3">
                             Join Us
                         </Button>
                     </div>
