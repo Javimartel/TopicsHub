54ea1bdb1d966541308c8013af874b19d685ec79
Javi <javiermespino@gmail.com>
2023-04-26 13:48:10 +0000

    Moved Spinner to a component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -12,6 +12,7 @@ import Message from "./chat/Message";
 import SendMessage from "./chat/SendMessage";
 import SignIn from "./chat/LogIn";
 import LogOut from "./chat/LogOut";
+import Spinner from "./Spinner";
 import Sidebar from "./chat/Sidebar";
 // Custom hook
 import { useSpinner } from "./hooks/useSpinner";
@@ -95,13 +96,7 @@ const Chat = () => {
             </div>
             {/* Añadimos el spinner de 1,5s para que se muestre antes que el main */}
             {showSpinner ? (
-                <div className="spin">
-                    <div className="m-10 flex justify-center items-center">
-                        <svg className="animate-spin spin" fill="none" height="150" viewBox="0 0 20 20" width="150" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 3.5C6.41015 3.5 3.5 6.41015 3.5 10C3.5 10.4142 3.16421 10.75 2.75 10.75C2.33579 10.75 2 10.4142 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C9.58579 18 9.25 17.6642 9.25 17.25C9.25 16.8358 9.58579 16.5 10 16.5C13.5899 16.5 16.5 13.5899 16.5 10C16.5 6.41015 13.5899 3.5 10 3.5Z" fill="#000000" />
-                        </svg>
-                    </div>
-                </div>
+                <Spinner />
             ) : (
                 <main>
                     {/* Comprobamos si está vacío el chat para redirigir a 404 */}
