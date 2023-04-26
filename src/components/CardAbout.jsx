da7e578ef522e103e62bce7e0ed073fea4168d5e
Javi <javiermespino@gmail.com>
2023-04-26 14:35:58 +0000

    Modified width

--- src/components/CardAbout.jsx
+++ src/components/CardAbout.jsx
@@ -34,10 +34,12 @@ const CardAbout = (props) => {
     //     </Typography>
     //   </CardBody>
     // </Card>
-    <div className="card lg:card-side bg-blue-500 text-black hover:text-white shadow-xl m-5">
-      <div className="min-w-[30%] max-w-[30%] min-h-[20vh] max-h-full"><img className="h-full min-w-full object-cover" src={props.img} alt={props.theme} /></div>
+    <div className="card lg:card-side bg-blue-500 text-black hover:text-white shadow-xl m-5 min-w-[25%]">
+      <div className="min-w-[30%] max-w-[30%] min-h-[20vh] max-h-full">
+          <img className="h-full min-w-full object-cover" src={props.img} alt={props.name} />
+        </div>
       <div className="card-body flex items-center">
-        <h2 className="card-title">{props.theme}</h2>
+        <h2 className="card-title">{props.name}</h2>
         <p className="text-last-center">{props.description}</p>
         <p>{props.category}</p>
       </div>
