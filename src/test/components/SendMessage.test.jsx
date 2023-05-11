2cde9077627364768838550a8d0b4196dd855238
Javi <javiermespino@gmail.com>
2023-05-11 14:22:49 +0100

    Formatted Test

--- src/test/components/SendMessage.test.jsx
+++ src/test/components/SendMessage.test.jsx
@@ -1,43 +1,43 @@
 import React from "react";
 import { describe } from "vitest";
+import { getByRole } from "@testing-library/react";
 import { renderWithMockFirebase } from "../test-utils.jsx";
 
 import SendMessage from "../../components/chat/SendMessage.jsx";
-import { getByRole } from "@testing-library/react";
 
 describe("SendMessage", () => {
-    it("renders correctly", () => {
-        const { container } = renderWithMockFirebase(<SendMessage />);
+  it("renders correctly", () => {
+    const { container } = renderWithMockFirebase(<SendMessage />);
 
-        expect(container).toBeInTheDocument();
-        expect(container).not.toBeEmptyDOMElement()
-    });
+    expect(container).toBeInTheDocument();
+    expect(container).not.toBeEmptyDOMElement();
+  });
 
-    it("renders the input", () => {
-        const { container } = renderWithMockFirebase(<SendMessage />);
-        const input = getByRole(container, "textbox");
+  it("renders the input", () => {
+    const { container } = renderWithMockFirebase(<SendMessage />);
+    const input = getByRole(container, "textbox");
 
-        expect(input).toBeInTheDocument();
-        expect(input).toHaveAttribute("type", "text");
-        expect(input).toHaveAttribute("maxlength", "255");
-    });
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("type", "text");
+    expect(input).toHaveAttribute("maxlength", "255");
+  });
 
-    it("renders the button", () => {
-        const { container } = renderWithMockFirebase(<SendMessage />);
-        const button = getByRole(container, "button");
+  it("renders the button", () => {
+    const { container } = renderWithMockFirebase(<SendMessage />);
+    const button = getByRole(container, "button");
 
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveAttribute("type", "submit");
-    });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute("type", "submit");
+  });
 
-    it("sends a message", () => {
-        const { container } = renderWithMockFirebase(<SendMessage />);
-        const input = getByRole(container, "textbox");
-        const button = getByRole(container, "button");
+  it("sends a message", () => {
+    const { container } = renderWithMockFirebase(<SendMessage />);
+    const input = getByRole(container, "textbox");
+    const button = getByRole(container, "button");
 
-        input.value = "Test message";
-        button.click();
+    input.value = "Test message";
+    button.click();
 
-        expect(input.value).toBe("Test message");
-    });
+    expect(input.value).toBe("Test message");
+  });
 });
